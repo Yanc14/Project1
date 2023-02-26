6d1d84fccfd9acef13a4c37df7d7ce1a03568908
yang-cong <3099160167@qq.com>
2023-02-26 20:27:27 +0800

    第四次提交

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #include<string>
 #define MAX 1000
-//1、菜单界面
+//1、展示菜单
 void showMenu()
 {
     cout << "***************************" << endl;
@@ -15,10 +15,10 @@ void showMenu()
     cout << "*****  0、退出通讯录  *****" << endl;
     cout << "***************************" << endl;
 }
-//设计联系人与通讯录的结构体
+//创建联系人和通讯录结构体
 struct Person
 {
-    string m_name;
+    string m_Name;
     int m_Sex;
     int m_Age;
     string m_Phone;
@@ -26,27 +26,115 @@ struct Person
 };
 struct Addressbooks
 {
-    //创建通讯录保存的联系人数组，数组元素为struct
+    //创建通讯录结构体
     struct Person personArray[MAX];
     int m_Size;
 };
+//2、添加联系人
+void Addperson(Addressbooks *abs)
+{
+    if(abs->m_Size==MAX)
+    {
+        cout<<"通讯录已满，添加失败！"<<endl;
+        return;
+    }
+    else
+    {
+        //添加联系人信息
+        //姓名
+        string name;
+        cout<<"请输入姓名:"<<endl;
+        cin>>name;
+        abs->personArray[abs->m_Size].m_Name=name;
+        //性别
+        int sex=0;
+        cout << "请输入性别：" << endl;
+        cout << "1------男" << endl;
+        cout << "2------女" << endl;
+        while(true)
+        {
+            cin >> sex;
+            if(sex==1||sex==2)
+            {
+                abs->personArray[abs->m_Size].m_Sex = sex;
+                break;
+            }
+            else
+            {
+                cout<<"请重新输入："<<endl;
+            }
+        }
+        
+        //年龄
+        cout<<"请输入年龄"<<endl;
+        int age=0;
+        cin>>age;
+        abs->personArray[abs->m_Size].m_Age=age;
+        
+        //手机号
+        cout << "请输入手机号" << endl;
+        int phone = 0;
+        cin >> phone;
+        abs->personArray[abs->m_Size].m_Phone = phone;
+        
+        // 地址
+        cout << "请输入地址" << endl;
+        string address;
+        cin >> address;
+        abs->personArray[abs->m_Size].m_Address = address;
 
+        //更新用户数
+        abs->m_Size++;
 
+        cout<<"添加成功"<<endl;
+
+        system("pause");//暂停等待任意输入
+        system("cls");//清空屏幕
+    }
+};
+//3、显示联系人
+void Showperson(Addressbooks *abs)
+{
+    //显示所有联系人
+    //判断通讯录中的人数是否为零
+    if(abs->m_Size==0)
+    {
+        cout<<"通讯录为空！"<<endl;
+    }
+    else
+    {
+        for(int i=0;i<abs->m_Size;i++)
+        {
+            cout << "姓名："  <<abs->personArray[i].m_Name << endl;
+            cout << "性别：" << abs->personArray[i].m_Sex << endl;
+            cout << "年龄：" << abs->personArray[i].m_Age << endl;
+            cout << "电话：" << abs->personArray[i].m_Phone << endl;
+            cout << "住址：" << abs->personArray[i].m_Address<< endl;
+        }
+    }
+
+    system("pause");
+    system("cls");
+};
 
 int main()
 {
+
+    //创建通讯录实体
+    Addressbooks abs;
+    //初始化
+    abs.m_Size=0;;
     while(true){
         showMenu();//展示菜单
-
-        int select = 0; // 创建用户输入的选择变量
+        int select = 0; //选择功能
         cin >> select;
         switch (select)
         {
-        case 1: // 1、添加联系人
-            /* code */
+        case 1: // 添加联系人
+            Addperson(&abs);
             break;
-        case 2: // 2、显示联系人
-            /* code */
+        case 2: // 显示联系人
+            Showperson(&abs);
             break;
         case 3:
             /* code */
@@ -61,9 +149,8 @@ int main()
             /* code */
             break;
         case 0:
-            cout << "您已退出通讯录，欢迎下次使用！" << endl;
-            system("pause");
-            return 0;//退出循环
+            cout << "您已退出通讯录" << endl;
+            return 0;//直接退出main循环
             break;
         default:
             break;
